eb1743d702dc7ff20a1fcc4b319aaea2d42caf4e
Alberto-Bruno <brunoalberto97@outlook.it>
2023-02-09 17:38:40 +0100

    Chiamata axios api.php style Albums

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const app = Vue.createApp({
     name: "AlbumSpotify",
     data() {
-
+        return {
+            dischi: [],
+        }
+    },
+    created() {
+        axios.get('http://localhost:80/php-dischi-json/api.php').then((res) => {
+            this.dischi = res.data;
+        })
     }
 })
+app.mount("#app")
 
 
 
